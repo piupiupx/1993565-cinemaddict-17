636996de4709fa92981901fbf9a7071467afd97a
piupiupx <akikokuroki1@gmail.com>
2022-05-27 20:21:47 +0300

    исправление

--- src/presenter/card-list-presenter.js
+++ src/presenter/card-list-presenter.js
@@ -32,7 +32,9 @@ export default class CardListPresenter {
       render(this.#loadMoreButtonComponent, filmsListContainer );
       this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
     }
-
+    if (!this.#boardFilms.length) {
+      render(new EmptyListView(), this.filmsListContainer);
+    }
   };
 
   #handleLoadMoreButtonClick = (evt) => {
@@ -60,7 +62,7 @@ export default class CardListPresenter {
     };
 
     const replaceFormToPopup = () => {
-      this.#filmsListComponent.element.replaceChild( filmComponent.element, popupComponent.element);
+      this.#filmsListComponent.element.removeChild(popupComponent.element);
     };
     const onEscKeyDown = (evt) => {
       if (evt.key === 'Escape' || evt.key === 'Esc') {
