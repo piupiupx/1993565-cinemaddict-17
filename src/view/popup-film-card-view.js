de0fa5293b217fbc8be8f1db860d724afc50017c
piupiupx <akikokuroki1@gmail.com>
2022-06-14 16:55:18 +0300

    Исправления

--- src/view/popup-film-card-view.js
+++ src/view/popup-film-card-view.js
@@ -40,17 +40,13 @@ const createCommentsTemplate = (comments) =>
     .map(
       (comment) => `<li class="film-details__comment" >
     <span class="film-details__comment-emoji">
-      <img src="./images/emoji/${
-        comment.emotion
-      }.png" width="55" height="55" alt="emoji-smile">
+      <img src="./images/emoji/${comment.emotion}.png" width="55" height="55" alt="emoji-smile">
     </span>
     <div>
       <p class="film-details__comment-text">${comment.comment}</p>
       <p class="film-details__comment-info">
         <span class="film-details__comment-author">${comment.author}</span>
-        <span class="film-details__comment-day">${humanizeCommentDate(
-          comment.date
-        )}</span>
+        <span class="film-details__comment-day">${humanizeCommentDate(comment.date)}</span>
         <button class="film-details__comment-delete">Delete</button>
       </p>
     </div>
